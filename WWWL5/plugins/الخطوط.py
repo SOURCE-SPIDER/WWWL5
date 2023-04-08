9f536ef5361d6697211d3f017c850e413e25ca10
SOURCE SPIDER <117318674+SOURCE-SPIDER@users.noreply.github.com>
2023-04-08 17:40:29 +0200

    Add files via upload

---
+++ WWWL5/plugins/الخطوط.py
@@ -0,0 +1,49 @@
+# Reda - Hussein
+# © WWWL5 Team 2023
+# ها شعدك داخل ع الملف تريد تخمط ؟ ابو زربة لهل درجة فاشل  
+from telethon import events
+from WWWL5 import WWWL5
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from ..core.managers import edit_delete
+from telethon import functions
+from telethon.errors.rpcerrorlist import MessageIdInvalidError
+@WWWL5.on(admin_cmd(pattern="(خط الغامق|خط غامق)"))
+async def btext(event):
+    isbold = gvarstatus("bold")
+    if not isbold:
+        addgvar ("bold", "on")
+        await edit_delete(event, "**᯽︙ تم تفعيل خط الغامق بنجاح ✓**")
+        return
+
+    if isbold:
+        delgvar("bold")
+        await edit_delete(event, "**᯽︙ تم اطفاء خط الغامق بنجاح ✓ **")
+        return
+
+@WWWL5.on(admin_cmd(pattern="(خط رمز|خط الرمز)"))
+async def btext(event):
+    isramz = gvarstatus("ramz")
+    if not isramz:
+        addgvar ("ramz", "on")
+        await edit_delete(event, "**᯽︙ تم تفعيل خط الرمز بنجاح ✓**")
+        return
+
+    if isramz:
+        delgvar("ramz")
+        await edit_delete(event, "**᯽︙ تم اطفاء خط الرمز بنجاح ✓ **")
+        return
+
+@WWWL5.on(events.NewMessage(outgoing=True))
+async def reda(event):
+    isbold = gvarstatus("bold")
+    if isbold:
+        try:
+            await event.edit(f"**{event.message.message}**")
+        except MessageIdInvalidError:
+            pass
+    isramz = gvarstatus("ramz")
+    if isramz:
+        try:
+            await event.edit(f"`{event.message.message}`")
+        except MessageIdInvalidError:
+            pass
